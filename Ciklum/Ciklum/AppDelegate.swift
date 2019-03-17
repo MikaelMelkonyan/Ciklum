7afafda09100129684e8c529475a053cd062271e
Mikael <melkonyan2311@mail.ru>
2019-03-17 11:41:18 +0200

    Project was creted

---
+++ Ciklum/Ciklum/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Ciklum
+//
+//  Created by Mikael Melkonyan on 3/17/19.
+//  Copyright © 2019 Mikael Melkonyan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
